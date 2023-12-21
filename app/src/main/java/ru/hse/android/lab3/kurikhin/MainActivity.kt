530289bcf837e55c0e491674f62caccee4d8ca98
Ivan Kurikhin <ivkurikhin@edu.hse.ru>
2023-12-21 23:20:14 +0300

    Initial commit

---
+++ app/src/main/java/ru/hse/android/lab3/kurikhin/MainActivity.kt
@@ -0,0 +1,46 @@
+package ru.hse.android.lab3.kurikhin
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.hse.android.lab3.kurikhin.ui.theme.NewsFetcherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NewsFetcherTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NewsFetcherTheme {
+        Greeting("Android")
+    }
+}
