aaee0bdfb4889b783e297b378b3387a25739573c
Ivan Kurikhin <ivkurikhin@edu.hse.ru>
2023-12-25 19:14:54 +0300

    Define network service

---
+++ app/src/main/java/ru/hse/android/lab3/kurikhin/network/NewsApiService.kt
@@ -0,0 +1,25 @@
+package ru.hse.android.lab3.kurikhin.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+import ru.hse.android.lab3.kurikhin.models.News
+
+private const val API_KEY = ""
+private const val BASE_URL = "https://newsdata.io/api/1/news"
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface NewsApiService {
+    @GET("/")
+    fun getNews(query: String): ArrayList<News>
+}
+
+object NewsApi {
+    val retrofitService : NewsApiService by lazy {
+        retrofit.create(NewsApiService::class.java)
+    }
+}
--- app/src/main/java/ru/hse/android/lab3/kurikhin/ui/screens/main/MainScreen.kt
+++ app/src/main/java/ru/hse/android/lab3/kurikhin/ui/screens/main/MainScreen.kt
@@ -1,17 +1,11 @@
 package ru.hse.android.lab3.kurikhin.ui.screens.main
 
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import ru.hse.android.lab3.kurikhin.R
 import ru.hse.android.lab3.kurikhin.mockNews
